7fd1f21a6840c7279a27634efc121fac89577a39
Hasnain-raza12 <hasnainr896@gmail.com>
2026-09-23 12:52:27 +0500

    Test CI Failure

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-ThisWillCauseAnError
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
